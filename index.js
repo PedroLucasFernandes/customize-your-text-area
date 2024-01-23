892ec2fc858597d2a296cbac317985dc155d5f09
Pedro Lucas Fernandes <plff201100@gmail.com>
2024-01-23 20:34:36 -0300

    feat: add import and export settings

--- index.js
+++ index.js
@@ -68,3 +68,73 @@ function updateLocalStorage() {
     localStorage.setItem("text", text.value);
     localStorage.setItem("form", JSON.stringify(form));
 }
+
+const importButton = document.getElementById("importButton");
+const importModal = document.getElementById("importModal");
+const closeExportModal = document.getElementById("closeExportModal");
+
+const exportButton = document.getElementById("exportButton");
+const exportModal = document.getElementById("exportModal");
+const closeImportModal = document.getElementById("closeImportModal");
+
+
+importButton.addEventListener("click", function(){
+    importModal.style.display = "flex";
+    importModal.style.position = "absolute";
+    importModal.style.left = "50vw";
+    importModal.style.top = "50vh";
+});
+
+exportButton.addEventListener("click", function(){
+    exportModal.style.display = "flex";
+    exportModal.style.position = "absolute";
+    exportModal.style.left = "50vw";
+    exportModal.style.top = "50vh";
+
+    const exportCode = document.getElementById("exportCode");
+    
+    form.text = text.value;
+
+    exportCode.innerText = JSON.stringify(form, null, 2);
+});
+
+closeImportModal.addEventListener("click", function(){
+    importModal.style.display = "none";
+
+    const importCode = document.getElementById("importCode");
+    try {
+        const importedForm = JSON.parse(importCode.value);
+        form.fontSize = importedForm.fontSize;
+        form.fontFamily = importedForm.fontFamily;
+        form.fontColor = importedForm.fontColor;
+        form.bgColor = importedForm.bgColor;
+        form.text = importedForm.text;
+        updatePageWithForm();
+    } catch (error) {
+        alert("Error parsing the imported JSON.");
+    }
+});
+
+closeExportModal.addEventListener("click", function(){
+    exportModal.style.display = "none";
+});
+
+function updatePageWithForm() {
+    text.style.fontSize = form.fontSize + "px";
+    text.style.fontFamily = form.fontFamily;
+    text.style.backgroundColor = form.bgColor;
+    text.style.color = form.fontColor;
+
+    fontSize.value = form.fontSize;
+    fontFamily.value = form.fontFamily;
+    bgColor.value = form.bgColor;
+    fontColor.value = form.fontColor;
+
+    text.value = form.text;
+    updateLocalStorage();
+}
+
+function updateLocalStorage() {
+    localStorage.setItem("text", text.value);
+    localStorage.setItem("form", JSON.stringify(form));
+}
